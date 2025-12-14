250bbb381b9aa0eaa7abb070a3e31d18dfcb69dd
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-12-14 16:55:12 +0000

    Create setup_client.sh

---
+++ cilium/setup_client.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run -d --init --privileged --name client --network client ghcr.io/hellt/network-multitool
+docker exec client sh -c "ip route add 10.20.0.0/16 via 192.168.20.101 dev eth0 && ip route add 172.20.0.0/16 via 192.168.20.101 dev eth0"
+docker exec client ip route add 172.30.0.0/16 via 192.168.20.101 dev eth0
