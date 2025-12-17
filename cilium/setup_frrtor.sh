a0ad9a03e5394e4655ed8ec360e6f974fcc9af7e
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-12-17 01:43:16 +0000

    Update setup_frrtor.sh

--- cilium/setup_frrtor.sh
+++ cilium/setup_frrtor.sh
@@ -66,9 +66,11 @@ docker run -d --init --privileged --name frrtor \
 -v ./configure/frrtor.conf:/etc/frr/frr.conf \
 -v ./configure/frrdaemons:/etc/frr/daemons \
 -v ./configure/vtysh.conf:/etc/frr/vtysh.conf \
---network client \
---network kind01 \
---network kind02 \
+--network client --ip 192.168.20.101 \
+frrouting/frr:latest
+
+docker network connect --ip 10.20.0.101 kind01 frrtor
+docker network connect --ip 172.20.0.101 kind02 frrtor
 
 docker exec frrtor sh -c "sysctl -w net.ipv4.ip_forward=1 && sysctl -w net.ipv4.fib_multipath_hash_policy=1"
 docker exec frrtor vtysh -c "show interface && show running-config"
