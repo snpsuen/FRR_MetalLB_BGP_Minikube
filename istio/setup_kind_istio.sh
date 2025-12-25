22a012802d3a933531f61a3d6589dc056a47dde6
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-12-25 01:35:17 +0000

    Update setup_kind_istio.sh

--- istio/setup_kind_istio.sh
+++ istio/setup_kind_istio.sh
@@ -61,7 +61,3 @@ EOF
 
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml
-
-
-
-kubectl taint nodes istio-control-plane node-role.kubernetes.io/control-plane:NoSchedule-
